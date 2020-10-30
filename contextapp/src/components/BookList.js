b3b680a472cda44ba882debef3e2cdcedeea5041
Helen Gu <helengu1996@gmail.com>
2020-10-30 15:47:58 -0400

    Created project booklist

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
 import { ThemeContext } from '../context/ThemeContext';
 
 function BookList() {
 	const { isLightTheme, light, dark } = useContext(ThemeContext);
+	const { books } = useContext(BookContext);
 	const theme = isLightTheme ? light : dark;
 	return (
 		<div
@@ -10,9 +12,13 @@ function BookList() {
 			style={{ background: theme.bg, color: theme.syntax }}
 		>
 			<ul>
-				<li style={{ background: theme.ui }}>the way of kings</li>
-				<li style={{ background: theme.ui }}>the name of the wind</li>
-				<li style={{ background: theme.ui }}>the final empire</li>
+				{books.map((book) => {
+					return (
+						<li key={book.id} style={{ background: theme.ui }}>
+							{book.title}
+						</li>
+					);
+				})}
 			</ul>
 		</div>
 	);
